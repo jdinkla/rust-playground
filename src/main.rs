ede6be86b4519f29b8cd42dbff1f360635a544da
Jörn Dinkla <jdinkla@thoughtworks.com>
2020-12-04 13:49:18 +0100

    struct formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+// Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+// is a structure which contains a single `i32`.
+#[derive(Debug)]
+struct Structure(i32);
+
+// Put a `Structure` inside of the structure `Deep`. Make it printable
+// also.
+#[derive(Debug)]
+struct Deep(Structure);
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
+
+fn format_debug() {
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name for {actor}.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+    
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
+    println!("Now {:#?} will print!", Deep(Structure(7)));
+
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{:#?}", peter);
+    println!("{:?}", peter);
+
+}
+
+use std::fmt;
+
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> { 
+        write!(f, "{}", self.0)
+    }
+}
+
+fn format_display() {
+
+    println!("{} has structure", Structure(123))
+}
+
 fn main() {
-    println!("Hello, world!");
+    format_debug();
+    format_display();
 }
